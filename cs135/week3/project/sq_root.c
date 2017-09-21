2600e7dd919329e802dc1566cff75134d753fb85
Matt Davis <mdavis.tles@gmail.com>
2017-09-21 12:00:48 -0700

    Begin work on sq_root.c -- on hold for lecture

---
+++ cs135/week3/project/sq_root.c
@@ -0,0 +1,19 @@
+/*
+ * Name: sq_root.c
+ * Purpose: Use a loop to calculate the square root of user's input
+ * Author: Matt Davis
+ * Created: 9/21/17
+ * Last Updated: 9/21/17
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    double n;
+
+    printf("Enter a number: ");
+    scanf("%lf", &n);
+
+    return 0;
+}
