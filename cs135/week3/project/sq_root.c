6bf5b9215383b387aaad8cde78ebeec840b06d95
Matt Davis <mdavis.tles@gmail.com>
2017-09-25 22:23:54 -0700

    Finally got the project running correctly

--- cs135/week3/project/sq_root.c
+++ cs135/week3/project/sq_root.c
@@ -3,33 +3,28 @@
  * Purpose: Use a loop to calculate the square root of user's input
  * Author: Matt Davis
  * Created: 9/21/17
- * Last Updated: 9/22/17
+ * Last Updated: 9/25/17
  */
 
 #include <stdio.h>
 #include <math.h>
 
-#define GOOD_ENOUGH 0.00001
+#define GOOD_ENOUGH 1e-5
 
 int main(void)
 {
-    double n, root, guess = 1.0;
+    double n, guess = 1.0;
 
     printf("Enter a number: ");
     scanf("%lf", &n);
 
-    // Calc actual root of input
-    root = sqrt(n);
-
-    // Always print 1.0 as first guess
-    printf("%10.5lf\n", guess);
-
-    do {
-
+    while (1) {
         guess = (guess + (n / guess)) / 2.0;
+        if (fabs(n - (guess * guess)) < GOOD_ENOUGH) {
+            break;
+        }
         printf("%10.5lf\n", guess);
-
-    } while(guess > root + GOOD_ENOUGH || guess < root - GOOD_ENOUGH);
+    }
 
     printf("Estimated square root of %.5lf: %.5lf\n", n, guess);
 
