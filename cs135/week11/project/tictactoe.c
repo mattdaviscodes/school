12b377ae5b08a9602cdd33a6694dc97f1da6d767
Matt Davis <mdavis.tles@gmail.com>
2017-11-16 11:55:40 -0800

    Begin work on project7

---
+++ cs135/week11/project/tictactoe.c
@@ -0,0 +1,313 @@
+/*
+ * Name: tictactoe.c
+ * Purpose: Implement simple Tic Tac Toe clone to practice functions,
+ *          arrays, and pointers.
+ * Author: Matt Davis
+ * Created: 11/14/17
+ * Last Updated: 11/14/17
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define ROWS 3
+
+// Prototypes
+void clear_table(int board[ROWS][ROWS]);
+void generate_player2_move(int board[ROWS][ROWS], int *row, int *col);
+bool check_table_full(int board[ROWS][ROWS]);
+int check_three_in_a_row(int board[ROWS][ROWS]);
+void display_table(int board[ROWS][ROWS]);
+bool check_legal_option(int board[ROWS][ROWS], int row, int col);
+void update_table(int board[ROWS][ROWS], int row, int col, int value);
+
+void print_intro(void);
+char int_to_symbol(int key);
+int rand_0_2(void);
+void init_rand(void);
+
+
+
+int main(void)
+{
+    int board[ROWS][ROWS];
+    int x, y, *xp = &x, *yp = &y;
+
+    // Initial setup
+    init_rand();
+    clear_table(board);
+    print_intro();
+
+    while ( !check_table_full(board) && !check_three_in_a_row(board) ) {
+        display_table(board);
+        do {
+            printf("Player 1 enter your selection [row, col]");
+            scanf("%d, %d", &x, &y);
+        } while( !check_legal_option(board, x-1, y-1) );
+        update_table(board, x-1, y-1, 1);
+    }
+
+    return 0;
+}
+
+/*
+ *  Function: print_intro
+ *  ---------------------
+ *      Prints a welcome message to the console.
+ */
+void print_intro(void)
+{
+    printf("This program plays the game of tic-tac-toe\n");
+}
+
+/*
+ *  Function: clear_table
+ *  ---------------------
+ *      Clears the game board for a new game.
+ *
+ *      board: a 2d char array to be cleared
+ */
+void clear_table(int board[ROWS][ROWS])
+{
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            board[i][j] = 0;
+        }
+    }
+}
+
+/*
+ *  Function: generate_player2_move
+ *  ---------------------
+ *      Choses random moves on a game board and returns once a valid move
+ *      is found.
+ *
+ *      board: a 2d char array to be printed
+ *      row: int pointer to be filled with the x coordinate of generated move
+ *      row: int pointer to be filled with the y coordinate of generated move
+ */
+void generate_player2_move(int board[ROWS][ROWS], int *row, int *col)
+{
+    bool legal;
+
+    do {
+        *row = rand_0_2();
+        *col = rand_0_2();
+        legal = check_legal_option(board, *row, *col);
+    } while( !legal );
+}
+
+/*
+ *  Function: check_table_full
+ *  ---------------------
+ *      Checks if game board is full (if all ints in board are non-zero)
+ *
+ *      return: true if full, else false
+ */
+bool check_table_full(int board[ROWS][ROWS])
+{
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            if (board[i][j] == 0) {
+                // Found an empty square
+                return false;
+            }
+        }
+    }
+
+    // No empty squares found
+    return true;
+}
+
+/*
+ *  Function: check_three_in_a_row
+ *  ---------------------
+ *      Checks if either player controls a complete row, thus winning the game.
+ *      A "row" in this sense constitues three contiguous spaces which may run
+ *      horizontally, vertically, or diagonally.
+ *
+ *      return: 1 if the player wins, 2 if the computer wins, else 0
+ */
+int check_three_in_a_row(int board[ROWS][ROWS])
+{
+    int index;
+    int player_counts[ROWS] = {0};
+
+    // Check rows first
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            index = board[i][j];
+            player_counts[index]++;
+        }
+
+        if (player_counts[1] == 3) {
+            // Player wins
+            printf("A\n");
+            return 1;
+        } else if (player_counts[2] == 3) {
+            // Computer wins
+            printf("B\n");
+            return 2;
+        } else {
+            // No winner, reset counts
+            player_counts[0] = 0;
+            player_counts[1] = 0;
+            player_counts[2] = 0;
+        }
+    }
+
+    // Then check columns
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            index = board[j][i];
+            player_counts[index]++;
+        }
+
+        if (player_counts[1] == 3) {
+            // Player wins
+            printf("C\n");
+            return 1;
+        } else if (player_counts[2] == 3) {
+            // Computer wins
+            printf("D\n");
+            return 2;
+        } else {
+            // No winner, reset counts
+            player_counts[0] = 0;
+            player_counts[1] = 0;
+            player_counts[2] = 0;
+        }
+    }
+
+    // Finally check diagonals
+    if (board[0][0] == board[1][1] == board[2][2]) {
+        if (board[0][0] != 0) {
+            printf("E\n");
+            printf("%d\n", board[0][0] == board[1][1] == board[2][2]);
+            printf("%d %d %d\n", board[0][0], board[1][1], board[2][2]);
+            return board[0][0];
+        }
+    } else if (board[0][2] == board[1][1] == board[2][0]) {
+        if (board[0][2] != 0) {
+            printf("F\n");
+            return board[0][2];
+        }
+    }
+
+    // FINALLY, if no winner found, return 0
+    return 0;
+}
+
+/*
+ *  Function: update_table
+ *  ---------------------
+ *      Sets board[row][col] equal to value
+ *
+ *      board: a 2d char array to be printed
+ *      row: int x coordinate of board
+ *      col: in y coordinate of board
+ *      val: int value to place at coords -- 1 or 2 (player or computer)
+ */
+void update_table(int board[ROWS][ROWS], int row, int col, int value)
+{
+    board[row][col] = value;
+}
+
+/*
+ *  Function: display_table
+ *  ---------------------
+ *      Prints the current table state to the screen
+ *
+ *      board: a 2d char array to be printed
+ */
+void display_table(int board[ROWS][ROWS])
+{
+    printf("The current state of the game is:\n");
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < ROWS; j++) {
+            printf("%c ", int_to_symbol(board[i][j]));
+        }
+        printf("\n");
+    }
+}
+
+/*
+ *  Function: check_legal_option
+ *  ---------------------
+ *      Checks whether a move identified by board[row][col] is:
+ *          - within the bounds of the board
+ *          - unoccupied
+ *
+ *      board: a 2d char array to be printed
+ *      row: int x-axis index to board
+ *      col: int y-axis index to board
+ *
+ *      return: true if valid, else false
+ */
+bool check_legal_option(int board[ROWS][ROWS], int row, int col)
+{
+    _Bool valid = true;
+
+    // If move is outside game bounds, invalid
+    if (row + 1 > ROWS || col + 1 > ROWS) {
+        valid = false;
+    }
+
+    // Check spot unoccupied
+    if (board[row][col] != 0) {
+        valid = false;
+    }
+
+    return valid;
+}
+
+/*
+ *  Function: int_to_symbol
+ *  ---------------------
+ *      Converts an int 0-2 in the board array to the corresponding symbol.
+ *          0 = _
+ *          1 = X
+ *          2 = O
+ *
+ *      key: an integer 0 through 2
+ *
+ *      return: char _, X, or O
+ */
+char int_to_symbol(int key)
+{
+    char symbol;
+
+    switch (key) {
+        case 0:
+            symbol = '_';
+            break;
+        case 1:
+            symbol = 'X';
+            break;
+        case 2:
+            symbol = 'O';
+            break;
+    }
+
+    return symbol;
+}
+
+
+int rand_0_2(void)
+{
+    int num_generated;
+
+    num_generated = rand() % 3;
+
+    return num_generated;
+}
+
+void init_rand(void)
+{
+    time_t t;
+
+    // Init random number generator
+    srand((unsigned) time(&t));
+}
